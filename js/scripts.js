4462fdad595d27a4d40db50742e3a51821135249
makenzie-ops <laurenmakenzie0@gmail.com>
2020-02-14 11:11:44 +0300

    Add styles

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function Birthday() {
+    var date = parseInt(document.getElementsByName["date"].value);
+    var month = parseInt(document.getElementsByName["month"].value);
+    var year = parseInt(document.getElementsByName["year"].value);
+    
+}
