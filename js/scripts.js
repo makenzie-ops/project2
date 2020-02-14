5830389081eae6d460fe6e19d47fdf9e8b4baa9b
makenzie-ops <laurenmakenzie0@gmail.com>
2020-02-14 14:33:57 +0300

    Add styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,34 @@
-function Birthday() {
-    var date = parseInt(document.getElementsByName["date"].value);
-    var month = parseInt(document.getElementsByName["month"].value);
-    var year = parseInt(document.getElementsByName["year"].value);
-    var gender = document.getElementsByName["gender"].value;
 
-    if (date = 0 || date >31 ) {
-        alert("Error!Enter correctly!!")
+let submitDetails = document.getElementById('submit')
+
+submitDetails.addEventListener('click', submitBirthDay)
+function submitBirthDay(e) {
+    e.preventDefault();
+    var date = parseInt(document.getElementById("date").value);
+    var month = parseInt(document.getElementById("month").value);
+    var year = parseInt(document.getElementById("year").value);
+
+    var days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+    var Males = ["Kwasi", "Kwadwo", "Kwabena", " Kwaku", "Yaw", "Kofi", " Kwame"];
+    var Females = ["Akosua", " Adwoa", " Abenaa", "Akua", " Yaa", "Afua", "Ama"];
+
+    if (date <= 0 || date >31 ) {
+        alert("Error!Enter correctly!!");
     }
-        else (month = 0 || month >12 ) 
-            alert("Error! Enter correctly!!")
+    if(month <= 0 || month >12 ) {
+        alert("Error! Enter correctly!!");
+    }
 
-var days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"]
-var Males = ["Kwasi", "Kwadwo", "Kwabena", " Kwaku", "Yaw", "Kofi", " Kwame"]
-var Females = ["Akosua", " Adwoa", " Abenaa", "Akua", " Yaa", "Afua", "Ama"]
+    var day=new Date(year+"/"+ month+"/"+ date);
+    var Birthday=day.getDay();
 
-var day=new Date(year+"/"+ month+"/"+ date);
-var Birthday=day.getDay();
-
-if (Female.checked ==true ){
-    alert("You were born on "+days[Birthday]);
-    alert("Your name is "+Males[Birthday]);
-}
-else if (Males.checked ==true){
-         alert("You were born on"+days[Birthday]);
-         alert("Your name is "+Females[Birthday]);
-}
-        else (gender="")
-        alert("Error!!")
+    if (Female.checked ==true ){
+        alert("You were born on "+days[Birthday]);
+        alert("Your name is "+Males[Birthday]);
+    }
+    else if (Males.checked ==true){
+            alert("You were born on"+days[Birthday]);
+            alert("Your name is "+Females[Birthday]);
+    }
 
 }
