e34a65d90d8721b5bd8e0aefec51f4d0aea6b4c0
makenzie-ops <laurenmakenzie0@gmail.com>
2020-02-16 10:25:35 +0300

    add changes done

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,31 @@
-
+var days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+var males = ["Kwasi", "Kwadwo", "Kwabena", " Kwaku", "Yaw", "Kofi", " Kwame"];
+var females = ["Akosua", " Adwoa", " Abenaa", "Akua", " Yaa", "Afua", "Ama"];
 let submitDetails = document.getElementById('submit')
 
 submitDetails.addEventListener('click', submitBirthDay)
 function submitBirthDay(e) {
-    
     e.preventDefault();
-    var date = parseInt(document.getElementById("date").value);
     
+    var year = parseInt(document.getElementById("year").value);
     var month = parseInt(document.getElementById("month").value);
-    var year = document.getElementById("year").value;
-    var CC=parseInt(year.slice(0,2));
-    var YY=parseInt(year.slice(2,4));
-    var gender = document.getElementById("gender").value;
-    console.log (gender)
-
-    var days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
-    var males = ["Kwasi", "Kwadwo", "Kwabena", " Kwaku", "Yaw", "Kofi", " Kwame"];
-    var females = ["Akosua", " Adwoa", " Abenaa", "Akua", " Yaa", "Afua", "Ama"];
-
-    if (date <= 0 || date >31 ) {
-        alert("Error!Enter correctly!!");
-        return;
+    var date = parseInt(document.getElementById("date").value);
+    var gender =getGender ();
+    var date = newDate ( year +"/" + month + "/" + day);
+    var dayToBorn = date.getDate();
+    var akanName;
+    var validDate = (year > 0 && (month > 0 && month<= 12) && (date > 0 && day <= 31));
+    var validDateGenger = ( gender == "male"  && gender == "female");
+     
+    if ( month<= 0 || month >12) {
+        alert("Error! Enter correctly")
     }
-    else if(month <= 0 || month >12 ) {
-        alert("Error! Enter correctly!!");
-        return;
+    else if (date <=0 || date>=31) {
+        alert( "Invalid!!")
     }
-    else {
-        var Dayoftheweek = parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(month+1)/10)) + date ) % 7);
-        return Dayoftheweek;
+    else if (year =0) {
+        alert("Error!!")
     }
-
-    
-    //var day=new Date(year+"/"+ month+"/"+ date);
-    //var birthDay=day.getDay();
-    
-     var pickGender = document.getElementById("gender").value;
-
-    
-     if (pickGender == 0){
-        alert(days[Dayoftheweek]);
-        alert("Your name is "+Females[Dayoftheweek]);
-    }
-    else if (pickGender ==1){
-            alert(days[Dayoftheweek]);
-            alert("Your name is "+Males[Dayoftheweek]);
-    }
+    if (gende === "female " && year > 0 )
+     
 }
-
